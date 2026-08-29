8a538d2c1b13d04465e9b9b9f0bb8efa79bc83ca
Pedro Dias <pedrohenriqueherculanodias@gmail.com>
2026-08-28 22:41:44 -0300

    adicionei titulo no component da estatistica da semana

--- web/src/components/estatisticaSemana.jsx
+++ web/src/components/estatisticaSemana.jsx
@@ -15,13 +15,16 @@ function EstatisticaSemana() {
 
 
     return (
-        <ResponsiveContainer width="100%" height={300}>
-            <BarChart data={semana}>
-                <XAxis dataKey="dia" tickFormatter={(valor) => new Date(valor).toLocaleDateString('pt-BR', { weekday: 'short' })}   />
-                <YAxis tickFormatter={(valor) => `${valor}%`} />
-                <Bar dataKey="porcentagem" fill="#8884d8" />
-            </BarChart>
-        </ResponsiveContainer>
+        <div>
+            <h2>Estatistica da semana</h2>
+            <ResponsiveContainer width="100%" height={300}>
+                <BarChart data={semana}>
+                    <XAxis dataKey="dia" tickFormatter={(valor) => new Date(valor).toLocaleDateString('pt-BR', { weekday: 'short' })} />
+                    <YAxis tickFormatter={(valor) => `${valor}%`} />
+                    <Bar dataKey="porcentagem" fill="#8884d8" />
+                </BarChart>
+            </ResponsiveContainer>
+        </div>
     )
 }
 
