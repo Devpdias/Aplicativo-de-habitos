28a5d800165399e8c9aa7bce22ad4bf01c32211b
Pedro Dias <pedrohenriqueherculanodias@gmail.com>
2026-09-03 17:50:40 -0300

    coluna 'importante' adicionada a tabela habitos

---
+++ server/data/migrations/20260903203652_add_important_to_habitos.js
@@ -0,0 +1,19 @@
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> }
+ */
+exports.up = async function (knex) {
+  await knex.schema.alterTable("habitos", (table) => {
+    table.boolean("importante").defaultTo(false);
+  });
+};
+
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> }
+ */
+exports.down = async function (knex) {
+  await knex.schema.alterTable("habitos", (table) => {
+    table.dropColumn("importante");
+  });
+};
